0628f9dcd74fc7c3cf94702ab869d05218e2905c
RobRyan1122 <114590371+RobRyan1122@users.noreply.github.com>
2024-07-16 13:26:10 +0100

    Update settings.py

--- myproject/settings.py
+++ myproject/settings.py
@@ -125,6 +125,9 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/5.0/howto/static-files/
 
 STATIC_URL = 'static/'
+MEDIA_URL = '/media/'
+STATIC_ROOT = BASE_DIR / 'staticfiles'
+MEDIA_ROOT = BASE_DIR / 'media'
 
 
 # Default primary key field type
